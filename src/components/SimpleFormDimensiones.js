ffa2a9a0f2e6c9f188eb41f5dd372468f5a63edd
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-04-17 12:48:34 -0500

    index updated, and components folder added

---
+++ src/components/SimpleFormDimensiones.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form/immutable'
+import validate from '../validate'
+
+const renderField = ({ input, label, type, meta: { asyncValidating, touched, error } }) => (
+  <div>
+    <label>{label}</label>
+    <div className={asyncValidating ? 'async-validating' : ''}>
+      <input {...input} type={type} placeholder={label}/>
+      {touched && error && <span>{error}</span>}
+    </div>
+  </div>
+)
+
+const minimo = value => parseFloat(value.split(' ')[0]) > 0 ? value : -1 * parseFloat(value.split(' ')[0])
+
+const format = value => {
+	if (value && parseInt(value.split(' ')[0]) > 0 && value.includes('kg')) {
+		return value.split(' ')[0] + value.split(' ')[2] + ' kg ';
+	}
+	if (value) {
+		return value + ' kg ';	
+	}
+} 
+
+const formato = value => {
+	if (value && parseInt(value.split(' ')[0]) > 0 && value.includes('mm')) {
+		return value.split(' ')[0] + value.split(' ')[2] + ' mm ';
+	}
+	if (value) {
+		return value + ' mm ';	
+	}
+} 
+
+const people = value => {
+	if (value && parseInt(value.split(' ')[0]) > 0 && value.includes('people')) {
+		return value.split(' ')[0] + value.split(' ')[2] + ' people ';
+	}
+	if (value) {
+		return value + ' people ';	
+	}
+} 
+
+
+const SimpleFormD = (props) => {
+	const { handleSubmit } = props
+	return (
+		<form onSubmit = {handleSubmit}>
+		<br/>
+			<div>
+			<center>
+			<tr>DIMENSIONES</tr><br/>
+			</center>
+			</div>
+			
+			<div>
+			<label>Cargo_capacity</label>
+			<div>
+				<Field name="cargo_capacity" component={ renderField } format={ format } type="text" normalize={ minimo }/>
+			</div>
+
+			<label>Cargo_bay</label>
+			<div>
+				<Field name="cargo_l" component={ renderField } format={ formato } type="text" normalize={ minimo }/>
+				<Field name="cargo_w" component={ renderField } format={ formato } type="text" normalize={ minimo }/>
+				<Field name="cargo_h" component={ renderField } format={ formato } type="text" normalize={ minimo }/>
+			</div>
+
+			<label>Number_of_passengers</label>
+			<div>
+				<Field name="number_of_passengers" component={ renderField } format={ people } type="text" normalize={ minimo }/>
+			</div>
+			</div>
+		</form>
+	)
+}
+
+export default reduxForm({
+  form: 'simpleDimen',  // a unique identifier for this form
+  validate
+})(SimpleFormD)
--- src/index.js
+++ src/index.js
@@ -3,7 +3,65 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+
+import immutable from 'immutable';
+import { combineReducers } from 'redux-immutablejs';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { reducer as formReducer } from 'redux-form/immutable'
+
+
+import SimpleFormDimensiones from './components/SimpleFormDimensiones';
+
+
+import {
+  BrowserRouter as Router,
+  Route,
+  IndexRoute,
+  Link,
+  Switch
+} from 'react-router-dom'
+
+
+const Component = (props) => {
+	
+	return (
+		<div>
+			{ props.children }
+		</div>
+	);
+}
+
+// import VehiclesReducer from './reducers/VehiclesDim';
+
+
+const initialState = immutable.Map();
+
+const rootReducer = combineReducers({
+	form: formReducer,
+	//VehiclesDim: VehiclesReducer,
+});
+
+const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
+
+
 ReactDOM.render(
-  <App />,
+  <Provider store={ store }>
+  		<Router>
+	    <div>
+	      <ul id="menu">
+	        <li><Link to='dimensiones'>Dimensiones</Link></li>
+	      </ul>
+
+	      <h2> PROYECTO </h2>
+
+	      <hr/>
+	      <Switch>
+	      	<Route path="/dimensiones" component={ props => <Component {...props}><SimpleFormDimensiones/></Component> } />
+	      </Switch>
+	    </div>
+  </Router>
+  	</Provider>,
   document.getElementById('root')
 );
