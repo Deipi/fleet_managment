3677e5223dc8723124246a7049502be329f1fdaa
Alfredo Correa <alfredo_cc13@outlook.com>
2017-04-18 15:11:58 -0500

    create a new form

---
+++ src/components/FormCombustible.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form/immutable'
+import Select from 'react-select'
+import 'react-select/dist/react-select.css'
+
+var CreatableComb= React.createClass({
+	displayName: 'CreatableComb',
+	propTypes: {
+		hint: React.PropTypes.string,
+		label: React.PropTypes.string
+	},
+	getInitialState(){
+		return{
+			options: [
+				{ value: 'petrol', label: 'Petrol'},
+				{ value: 'diesel', label: 'Diesel'},
+				{ value: 'gas', label: 'Gas'}
+			]
+		};
+	},
+	handleOnChange(value){
+		const { options } = this.setState({ value });
+	},
+	render (){
+		const { options, value } = this.state;
+		return(
+			<div className="section">
+				<h3 className="section-heading">{this.props.label}</h3>
+				<Select.Creatable
+					options={options}
+					onChange={this.handleOnChange}
+					value={value}
+				/>
+			</div>
+		);
+	}
+});
+
+const minimo = value => parseFloat(value.split(' ')[0]) > 0 ? value : -1 * parseFloat(value.split(' ')[0])
+const format = value => {
+	if(value && parseInt(value.split(' ')[0]) > 0 && value.includes('L')){
+		return value.split(' ')[0]+value.split(' ')[2]+ ' L ';
+	}
+	if(value){
+		return value + ' L ';
+	}
+}
+
+const renderField = ({ input, label, type, meta: { asyncValidating, touched, error } }) => (
+  <div>
+    <label>{label}</label>
+    <div className={asyncValidating ? 'async-validating' : ''}>
+      <input {...input} type={type} placeholder={label}/>
+      {touched && error && <span>{error}</span>}
+    </div>
+  </div>
+)
+
+const FormCombustible=(props)=>{
+	const { handleSubmit } = props
+	return ( 
+		<div>
+		<br/>
+			<div>
+			<center>
+			<tr>FUEL</tr><br/>
+			</center>
+			</div>
+			
+			<div>
+			<label>Fuel type</label>
+			<div>
+				<Field name="fuel_type" component={ CreatableComb }  type="text"/>
+			</div>
+
+			<label>Fuel grade</label>
+			<div>
+				<Field name="fuel_grade" component="input" type="text"/>
+			</div>
+
+			<label>Fuel consumption</label>
+			<div>
+				<Field name="fuel_consumption" component={ renderField } format={ format }/>
+			</div>
+
+			<label>Tank capacity</label>
+			<div>
+				<Field name="tank_capacity" component={ renderField } format={ format }/>
+			</div>
+			</div>
+		</div>
+	)
+}
+export default FormCombustible
