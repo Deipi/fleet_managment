7699a36291ed4fc8791833f7d93097f903505756
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-04-21 11:05:39 -0500

    change

--- src/containers/VehiclesList.js
+++ src/containers/VehiclesList.js
@@ -7,12 +7,19 @@ import { Link } from 'react-router-dom';
 const NewLayout = ({ Table,Filter,Pagination }) => (
 	<div>
 		<Filter/>
-		<Link className="btn" tag={Link} color="info" to='/formulario'> Add Vehicle </Link>
-		<Link className="btn" tag={Link} color="info" to='/flotilla'> New Group </Link>
+		<Link className="btn" tag={Link} color="info" to='/hola'> Add Vehicle </Link>
+		<Link className="btn" tag={Link} color="info" to='/vehicles'> New Group </Link>
 		<Table/>
 		<Pagination/>
 	</div>
 );
+
+      		// <Link className="btn" tag={Link} to='/description'/>
+const CustomColumn = showDescription => ({value}) => 
+		<button type="button" className="btn btn-default" onClick={ () => showDescription(value) }>
+      		<span className="glyphicon glyphicon-align-justify"></span>
+    	</button>;
+
 
 const selector = state => {
 	return {
@@ -21,32 +28,66 @@ const selector = state => {
 };
 
 class VehiclesList extends Component {
+	constructor(props) {
+		super(props);
+
+		this.showDescription = this.showDescription.bind(this);
+
+		this.state = {
+			show: false,
+			data: 0,
+		};
+		
+	}
+
 	componentWillMount() {
 		const { props: { dispatch } } = this;
 		dispatch(fetchVehicles());
 	}
 
+	showDescription(value) {
+		this.setState({ show: true, data: value });
+	}
+
 	render() {
 		const { props: { vehicles } } = this;
+		const { show } = this.state;
+
+
+		const descriptionVehicle = vehicles.filter(obj => obj.id === this.state.data).toJS()[0]
 		return (
-			<Griddle data={ vehicles ? vehicles.toJS() : [] } 
-			plugins={[plugins.LocalPlugin]}
-			styleConfig={{
-				classNames: {
-					Table: 'table table-striped',
+			<div>
+				<Griddle data={ vehicles ? vehicles.toJS() : [] } 
+				plugins={[plugins.LocalPlugin]}
+				styleConfig={{
+					classNames: {
+						Table: 'table table-striped',
+					}
+				}}
+				components={{
+					Layout: NewLayout
+				}}>
+					<RowDefinition>
+					  <ColumnDefinition id="label" visible/>
+				      <ColumnDefinition id="model" visible/>
+				      <ColumnDefinition id="tracker" visible/>
+				      <ColumnDefinition id="reg_plate" visible/>
+				      <ColumnDefinition id="type" visible/>
+				      <ColumnDefinition id="id" title="Description" visible customComponent={ CustomColumn(this.showDescription) }/>
+				 	</RowDefinition>
+				</Griddle>
+				{ show && descriptionVehicle ? (
+					<div><br/><br/>
+						<div> <center><label>DESCRIPTION</label></center> </div>
+						<div> <center>{ descriptionVehicle.label }</center> </div>
+						<div> <center>{ descriptionVehicle.model }</center> </div>
+						<div> <center>{ descriptionVehicle.tracker }</center> </div>
+						<div> <center>{ descriptionVehicle.reg_plate }</center> </div>
+						<div> <center>{ descriptionVehicle.type }</center> </div>
+					</div>
+					) : null
 				}
-			}}
-			components={{
-				Layout: NewLayout
-			}}>
-				<RowDefinition>
-				  <ColumnDefinition id="label" visible/>
-			      <ColumnDefinition id="model" visible/>
-			      <ColumnDefinition id="tracker" visible/>
-			      <ColumnDefinition id="reg_plate" visible/>
-			      <ColumnDefinition id="type" visible/>
-			 	</RowDefinition>
-			</Griddle>
+			</div>
 		);
 	}
 } 
