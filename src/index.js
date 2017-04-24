3b38418660051aa2b73f2eefef07fea8a60b0cb5
Alfredo Correa <alfredo_cc13@outlook.com>
2017-04-24 10:29:59 -0500

    change index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
 
 import immutable from 'immutable';
 import { combineReducers } from 'redux-immutablejs';
@@ -15,6 +12,7 @@ import { reducer as formReducer } from 'redux-form/immutable'
 import FormUnidad from './components/FormUnidad';
 
 import SimpleFormFlotilla from './components/SimpleFormFlotilla';
+import FormConductor from './components/FormConductor';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -40,13 +38,14 @@ const Component = (props) => {
 }
 
 import vehicles from './reducers/VehiclesList';
-
+import unidades from './reducers/UnidadesList';
 
 const initialState = immutable.Map();
 
 const rootReducer = combineReducers({
 	form: formReducer,
 	VehiclesList: vehicles,
+	
 });
 
 const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
@@ -60,6 +59,7 @@ ReactDOM.render(
 	        <li><Link to='formulario'>Unidades</Link></li>
 	        <li><Link to='flotilla'>Flotilla</Link></li>
 	        <li><Link to='vehicles'>Vehicles</Link></li>
+	        <li><Link to='conductores'>Conductores</Link></li>
 	      </ul>
 
 	      <h2> PROYECTO </h2>
@@ -69,13 +69,21 @@ ReactDOM.render(
 	      	<Route path="/formulario" component={ props => 
 	      		<Component {...props}>
 	      		<FormUnidad initialValues={{label: 'Car', model: 'Ford Focus', vehicle_registration_plate: 'SBA 1234A', vin: 'JMZMA18P200411817', chassis_number: 'LJCPCBLCXI1000237'}}/></Component> } />
+	      	
 	      	<Route path="/flotilla" component={ props => 
 	      		<Component {...props}>
 	      		<SimpleFormFlotilla initialValues={ { nombre: 'Andrea', supervisor: 'Andres', garage: 'no garage' } } /></Component> } />
+	      	
 	      	<Route path="/vehicles" component={ props => 
-	      		<Component {...props}><VehiclesList/></Component>
-	      		}
-	      	/>
+	      		<Component {...props}>
+	      		<VehiclesList/></Component> } />
+	      	
+	      	<Route path="/conductores" component={ props => 
+	      		<Component {...props}>
+	      		<FormConductor initialValues={{last_name: 'Cartens', first_name: 'Jose', middle_name: 'Luis', hardware_key: 'JMZMA18P200411817', phone: '7861161212', email:'alf@g.com', driver_license_number: '123', driver_license_class: 'abc', expiration_date: '2017-04-21'}}/></Component> } />
+	      	
+	      	
+
 	      </Switch>
 	    </div>
   </Router>
