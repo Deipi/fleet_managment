6255e72fe691470e0333db2cebc835fb0eb88ffe
Alfredo Correa <alfredo_cc13@outlook.com>
2017-04-24 10:32:29 -0500

    component info

---
+++ src/components/FromInfoContacto.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form/immutable'
+
+const FormInfocontacto=(props)=>{
+	const { handleSubmit } = props
+	return(
+		<div>
+		<br/>
+			<div>
+				<center>
+					<tr>CONTACT INFO</tr>
+				</center>
+			</div>
+
+				<div>
+					<label>Phone</label>
+					<div>
+						<Field name="phone" component="input" type="number" min="10"/>
+					</div>
+
+					<label>E-mail</label>
+					<div>
+						<Field name="email" component="input" type="email"/>
+					</div>
+				</div>
+		</div>
+	)
+}
+export default FormInfocontacto;
