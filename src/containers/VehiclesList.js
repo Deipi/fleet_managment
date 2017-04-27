9ec4b6f272cd3a0ea22f97a098ac4af419effcdd
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-04-27 11:09:22 -0500

    change

--- src/containers/VehiclesList.js
+++ src/containers/VehiclesList.js
@@ -3,19 +3,19 @@ import { connect } from 'react-redux';
 import Griddle, { ColumnDefinition, RowDefinition, plugins } from 'griddle-react';
 import { fetchVehicles } from '../actions';
 import { Link } from 'react-router-dom';
-
+import { Vehicles } from '../components/Vehicles';
 const NewLayout = ({ Table,Filter,Pagination }) => (
 	<div>
 		<Filter/>
-		<Link className="btn" tag={Link} color="info" to='/formulario'> Add Vehicle </Link>
-		<Link className="btn" tag={Link} color="info" to='/flotilla'> New Group </Link>
+		<Link className="btn" tag={Link} color="info" to='/hola'> Add Vehicle </Link>
+		<Link className="btn" tag={Link} color="info" to='/vehicles'> New Group </Link>
 		<Table/>
 		<Pagination/>
 	</div>
 );
 
       		// <Link className="btn" tag={Link} to='/description'/>
-const CustomColumn = showDescription => ({value}) => 
+const CustomColumn = showDescription => ({value}) =>
 		<button type="button" className="btn btn-default" onClick={ () => showDescription(value) }>
       		<span className="glyphicon glyphicon-align-justify"></span>
     	</button>;
@@ -37,7 +37,6 @@ class VehiclesList extends Component {
 			show: false,
 			data: 0,
 		};
-		
 	}
 
 	componentWillMount() {
@@ -57,7 +56,7 @@ class VehiclesList extends Component {
 		const descriptionVehicle = vehicles.filter(obj => obj.id === this.state.data).toJS()[0]
 		return (
 			<div>
-				<Griddle data={ vehicles ? vehicles.toJS() : [] } 
+				<Griddle data={ vehicles ? vehicles.toJS() : [] }
 				plugins={[plugins.LocalPlugin]}
 				styleConfig={{
 					classNames: {
@@ -77,19 +76,17 @@ class VehiclesList extends Component {
 				 	</RowDefinition>
 				</Griddle>
 				{ show && descriptionVehicle ? (
-					<div><br/><br/>
-						<div> <center><label>DESCRIPTION</label></center> </div>
-						<div> <center>{ descriptionVehicle.label }</center> </div>
-						<div> <center>{ descriptionVehicle.model }</center> </div>
-						<div> <center>{ descriptionVehicle.tracker }</center> </div>
-						<div> <center>{ descriptionVehicle.reg_plate }</center> </div>
-						<div> <center>{ descriptionVehicle.type }</center> </div>
-					</div>
-					) : null
+					<Vehicles
+						label = { descriptionVehicle.label }
+						model = { descriptionVehicle.model }
+						tracker = { descriptionVehicle.tracker }
+						reg_plate = { descriptionVehicle.reg_plate }
+						type = { descriptionVehicle.type }
+					/> ) : null
 				}
 			</div>
 		);
 	}
-} 
+}
 
 export default connect(selector)(VehiclesList);
