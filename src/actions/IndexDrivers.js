fdd6676c2fecc10f869e0010e14a2d115c02483f
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-04-27 10:09:41 -0500

    new index

---
+++ src/actions/IndexDrivers.js
@@ -0,0 +1,23 @@
+export const POSTED_DRIVER = 'POSTED_DRIVER';
+export const FETCHED_DRIVERS = 'FETCHED_DRIVERS';
+
+export default data => (dispatch, getStore) => fetch('http://localhost:3005/drivers', {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json'
+	},
+	body: JSON.stringify(data)
+}).then( result => result.json().then( driver => dispatch({
+	type: POSTED_DRIVER,
+	payload: driver
+})));
+
+export const fetchDrivers = () => (dispatch, getStore) => fetch('http://localhost:3005/drivers', {
+	method: 'GET',
+	headers: {
+		'Content-Type': 'application/json'
+	},
+}).then( result => result.json().then( drivers => dispatch({
+	type: FETCHED_DRIVERS,
+	payload: drivers
+})));
