d1568f534295e24c2c48fc73385b356853b24fcf
Alfredo Correa <alfredo_cc13@outlook.com>
2017-04-27 10:44:36 -0500

    modified

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Map from './Monitoreo';
+import { getVehicles } from '../utils/ApiUtils.js'
 
 export default class MapContainer extends React.Component {
 
@@ -9,7 +10,33 @@ export default class MapContainer extends React.Component {
 		};
 	}
 
-	
+	componentWillMount() {
+		const plotVehicles = () => getVehicles().then((results) => {
+			const markers = results.data.map((item, index) => {
+				return {
+					position: {
+						//elementos del API
+						lat: item.address.geo.lat,
+						lng: item.address.geo.lng,
+					},
+					title: item.name,
+					key: index,
+					icon: {
+						path: window.google.maps.SymbolPath.CIRCLE,
+						scale: 2,
+						fillColor: '#000080',
+						strokeColor: '#000080',
+						fillOpacity: 1,
+						strokeOpacity: 1,
+					},
+				};
+			});
+			this.setState({ markers });
+		});
+
+		plotVehicles();
+		window.setInterval(plotVehicles, 2000);
+	}
 
 	render() {
 		return (
