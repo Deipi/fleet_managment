b0369b03ad64b49d6dbff50f39e56cfe8f5370e6
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-04-28 11:38:08 -0500

    new reducers

---
+++ src/reducers/EditList.js
@@ -0,0 +1,13 @@
+import Immutable from 'immutable';
+import { POSTED_EDIT, FETCHED_EDITED } from '../actions/indexEdit';
+
+export default (state=Immutable.List(), action) => {
+	switch(action.type) {
+		case POSTED_EDIT:
+			return state.merge(Immutable.List(action.payload));
+		case FETCHED_EDITED:
+			return Immutable.List(action.payload);
+		default:
+			return state;
+	}
+}
