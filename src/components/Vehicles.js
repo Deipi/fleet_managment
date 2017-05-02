af9917f8fa6e3f5b6ab72582a669cd9994219e2c
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-05-02 11:49:44 -0500

    change name to props

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -6,7 +6,7 @@ export const Vehicles = props => (
 		<div> <center>{ props.label }</center> </div>
 		<div> <center>{ props.model }</center> </div>
 		<div> <center>{ props.tracker }</center> </div>
-		<div> <center>{ props.reg_plate }</center> </div>
+		<div> <center>{ props.vehicle_registration_plate }</center> </div>
 		<div> <center>{ props.type }</center> </div>
 	</div>
 );
