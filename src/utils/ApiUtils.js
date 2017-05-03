308e9af7d74bc13971a0eff030b1ed2786df0df2
Alfredo Correa <alfredo_cc13@outlook.com>
2017-05-03 13:38:25 -0500

    change the url of axios

--- src/utils/ApiUtils.js
+++ src/utils/ApiUtils.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const getVehicles=()=>{
-  return axios.get('http://www.json-generator.com/api/json/get/cgacdBBhgy?indent=2');  
+  return axios.get('http://localhost:3004/empleados');
   //return axios.get('http://www.json-generator.com/api/json/get/cgacdBBhgy?indent=2');
 };
 
