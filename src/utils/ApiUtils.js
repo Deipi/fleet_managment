0ac0dfef2970f8b2c2c641c9c08977aeb82f6148
Alfredo Correa <alfredo_cc13@outlook.com>
2017-05-04 13:26:00 -0500

    delete commit

--- src/utils/ApiUtils.js
+++ src/utils/ApiUtils.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-
+import qs from 'qs'
 
 const getVehicles=()=>{
   return axios.get('http://localhost:3004/empleados');
-  //return axios.get('http://www.json-generator.com/api/json/get/cgacdBBhgy?indent=2');
 };
 
 export { getVehicles, };
