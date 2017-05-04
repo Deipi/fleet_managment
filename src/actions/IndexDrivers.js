6bed19eea1c7b75eb48104fc2e20900c5ce08990
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-05-04 14:09:45 -0500

    add method delete

--- src/actions/IndexDrivers.js
+++ src/actions/IndexDrivers.js
@@ -1,6 +1,7 @@
 export const POSTED_DRIVER = 'POSTED_DRIVER';
 export const FETCHED_DRIVERS = 'FETCHED_DRIVERS';
 export const FETCHED_EDITED = 'FETCHED_EDITED';
+export const DELETE_DRIVERS = 'DELETE_DRIVERS';
 
 export default data => (dispatch, getStore) => fetch('http://localhost:3004/conductores', {
 	method: 'POST',
@@ -21,4 +22,14 @@ export const fetchDrivers = () => (dispatch, getStore) => fetch('http://localhos
 }).then( result => result.json().then( conductores => dispatch({
 	type: FETCHED_DRIVERS,
 	payload: conductores
+})));
+
+export const deleteDrivers = (id) => (dispatch, getStore) => fetch(`http://localhost:3004/conductores/${ id }`, {
+	method: 'DELETE',
+	headers: {
+		'Content-Type': 'application/json'
+	},
+}).then( result => result.json().then( conductores => dispatch({
+	type: DELETE_DRIVERS,
+	payload: id
 })));
