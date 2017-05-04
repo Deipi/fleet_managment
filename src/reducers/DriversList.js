74f577f5ea0f52e496cb795826e3485fbde55d3a
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-05-04 14:14:23 -0500

    add delete drivers

--- src/reducers/DriversList.js
+++ src/reducers/DriversList.js
@@ -1,5 +1,5 @@
 import Immutable from 'immutable';
-import { POSTED_DRIVER, FETCHED_DRIVERS } from '../actions/IndexDrivers';
+import { POSTED_DRIVER, FETCHED_DRIVERS, DELETE_DRIVERS } from '../actions/IndexDrivers';
 
 export default (state=Immutable.List(), action) => {
 	switch(action.type) {
@@ -7,6 +7,8 @@ export default (state=Immutable.List(), action) => {
 			return state.merge(Immutable.List(action.payload));
 		case FETCHED_DRIVERS:
 			return Immutable.List(action.payload);
+		case DELETE_DRIVERS:
+			return state.filter(driver => driver.id !== action.payload);
 		default:
 			return state;
 	}
