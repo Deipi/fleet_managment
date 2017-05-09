fddced25eeed27f78b4b932081b1eaf5cf72228c
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-05-09 15:09:09 -0500

    import UPDATE_VEHICLES

--- src/reducers/VehiclesList.js
+++ src/reducers/VehiclesList.js
@@ -1,5 +1,5 @@
 import Immutable from 'immutable';
-import { POSTED_VEHICLE, FETCHED_VEHICLES, DELETE_VEHICLES } from '../actions';
+import { POSTED_VEHICLE, FETCHED_VEHICLES, DELETE_VEHICLES, UPDATE_VEHICLES } from '../actions';
 
 export default (state=Immutable.List(), action) => {
 	switch(action.type) {
@@ -9,6 +9,8 @@ export default (state=Immutable.List(), action) => {
 			return Immutable.List(action.payload);
 		case DELETE_VEHICLES:
 			return state.filter(vehicle => vehicle.id !== action.payload);
+		case UPDATE_VEHICLES:
+			return state.merge(Immutable.List(action.payload));
 		default:
 			return state;
 	}
