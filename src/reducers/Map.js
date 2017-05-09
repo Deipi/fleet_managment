bf86e7e14b2d0760e5e6bb470f603b125539a5df
Alfredo Correa <alfredo_cc13@outlook.com>
2017-05-09 16:03:50 -0500

    parse string to float

--- src/reducers/Map.js
+++ src/reducers/Map.js
@@ -5,8 +5,20 @@ export default (state=Immutable.fromJS([]), action) => {
 	switch(action.type) {
 		case FETCHED_EMPLEADOS:
 			return Immutable.fromJS(action.payload);
+
 		case GET_VEHICLES:
-			return Immutable.fromJS(action.payload);
+			const newList = action.payload.map(obj => {
+				if (typeof obj.latitud === "string") {
+					obj.latitud = parseFloat(obj.latitud);
+				}
+				if (typeof obj.longitud === "string") {
+					obj.longitud = parseFloat(obj.longitud);
+				}
+
+				return obj;
+			});
+
+			return Immutable.fromJS(newList);
 		default:
 			return state;
 	}
