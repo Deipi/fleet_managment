d55d77211fe267f6876ccf58c0a492fb3773a267
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-05-09 14:31:01 -0500

    delete spaces

--- src/components/FormUnidad.js
+++ src/components/FormUnidad.js
@@ -8,6 +8,7 @@ import FormCombustible from './FormCombustible';
 import FormWheelbase from './FormWheelbase';
 import FormSeguro from './FormSeguro';
 
+
 var CreatableTY = React.createClass({
   displayName: 'CreatableTY',
   propTypes: {
