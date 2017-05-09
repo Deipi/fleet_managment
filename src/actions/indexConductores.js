fe02c848aabe6efc6e43de2f73d223b8abde536a
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-05-09 14:27:33 -0500

    export const UPDATE_CONDUCTORES

--- src/actions/indexConductores.js
+++ src/actions/indexConductores.js
@@ -1,5 +1,6 @@
 export const POSTED_CONDUCTOR = 'POSTED_CONDUCTOR';
 export const FETCHED_CONDUCTORES = 'FETCHED_CONDUCTORES';
+export const UPDATE_CONDUCTORES = 'UPDATE_CONDUCTORES';
 
 export default data => (dispatch, getStore) => fetch('http://localhost:3004/conductores', {
 	method: 'POST',
@@ -19,5 +20,16 @@ export const fetchConductores=()=>(dispatch, getStore)=>fetch('http://localhost:
 	},
 }).then(result=>result.json().then(conductores=>dispatch ({
 	type: FETCHED_CONDUCTORES,
+	payload: conductores
+})));
+
+export const updateConductores= (id, data) =>(dispatch, getStore)=>fetch(`http://localhost:3004/conductores/${ id }`, {
+	method: 'PUT',
+	headers: {
+		'Content-Type': 'application/json'
+	},
+	body: JSON.stringify(data),
+}).then(result=>result.json().then(conductores=>dispatch ({
+	type: UPDATE_CONDUCTORES,
 	payload: conductores
 })));
