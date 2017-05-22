a936e9a697ba725f67906214a87c46ef52dd4467
Alfredo Correa <alfredo_cc13@outlook.com>
2017-05-22 15:33:32 -0500

    import fetched vehicles filter

--- src/reducers/Map.js
+++ src/reducers/Map.js
@@ -1,5 +1,6 @@
 import Immutable from 'immutable';
 import { FETCHED_EMPLEADOS, GET_VEHICLES } from '../actions/FilterMap';
+import { FETCHED_VEHICLES_FILTER } from '../actions/Filters';
 
 export default (state=Immutable.fromJS([]), action) => {
 	switch(action.type) {
@@ -7,8 +8,13 @@ export default (state=Immutable.fromJS([]), action) => {
 			return Immutable.fromJS(action.payload);
 
 		case GET_VEHICLES:
-		
 			return Immutable.fromJS(action.payload);
+
+		case FETCHED_VEHICLES_FILTER:
+			return Immutable.fromJS(action.payload);
+			// return state.filter(vehicle => vehicle.get('id') === action.payload[0].id);
+			// return state.filter(vehicle => vehicle.get('id') === action.payload[0].id);
+		
 		default:
 			return state;
 	}
