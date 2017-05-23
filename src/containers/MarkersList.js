887786d5c42a7c44e5624698439fe07662968dc6
Alfredo Correa <alfredo_cc13@outlook.com>
2017-05-23 14:02:54 -0500

    use griddle to table

--- src/containers/MarkersList.js
+++ src/containers/MarkersList.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import { fetchVehicles } from '../actions/index';
 
+import Griddle, { ColumnDefinition, RowDefinition, plugins } from 'griddle-react';
+
 const products = [{
 	id: 1,
 	model: 'Lobo',
@@ -12,11 +14,17 @@ const products = [{
 	status: 'Online'}
 ];
 
-// const selector = state => {
-// 	return {
-// 		unidades: state.get('VehiclesList'),
-// 	}
-// };
+const NewLayout = ({ Table }) => (
+	<div>
+		<Table/>
+	</div>
+);
+
+const selector = state => {
+	return {
+		unidades: state.get('VehiclesList'),
+	}
+};
 
 class DataTable extends Component {
 
@@ -29,17 +37,29 @@ class DataTable extends Component {
 
     return (
 
-      <BootstrapTable data={ unidades ? unidades.toJS() : [] } >
-          <TableHeaderColumn dataField='id' isKey={ true }>Unit ID</TableHeaderColumn>
-          <TableHeaderColumn dataField='model'>Model</TableHeaderColumn>
-          <TableHeaderColumn dataField='vehicle_registration_plate'>Plate</TableHeaderColumn>
-          <TableHeaderColumn dataField='type.label'>Type</TableHeaderColumn>
-          <TableHeaderColumn dataField='tracker'>Tracker</TableHeaderColumn>
-          <TableHeaderColumn dataField='status'>Status</TableHeaderColumn>
-      </BootstrapTable>
+     
+
+       <Griddle data={unidades ? unidades.toJS() : [] }
+       styleConfig={{
+					classNames: {
+						Table: 'table table-striped',
+					}
+		}}
+		components={{
+			Layout: NewLayout
+		}}>
+	    <RowDefinition>
+	      <ColumnDefinition id="id" title="Unit ID" visible />
+	      <ColumnDefinition id="model" title='Model' visible/>
+	      <ColumnDefinition id="vehicle_registration_plate" title='Plate' visible />
+	      <ColumnDefinition id="type.label" title='Type' visible />
+	      <ColumnDefinition id="tracker.label" title='Tracker' visible />
+	      <ColumnDefinition id="status.label" title='Status' visible/>
+	    </RowDefinition>
+	  </Griddle>
 
     );
   }
 }
 
-export default connect() (DataTable);
+export default connect(selector) (DataTable);
