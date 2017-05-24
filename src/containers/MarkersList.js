60199ea777a9a70ad2fbce84f6989eafc30ef770
Alfredo Correa <alfredo_cc13@outlook.com>
2017-05-24 13:59:28 -0500

    use Griddle to make a table

--- src/containers/MarkersList.js
+++ src/containers/MarkersList.js
@@ -5,13 +5,24 @@ import { fetchVehicles } from '../actions/index';
 
 import Griddle, { ColumnDefinition, RowDefinition, plugins } from 'griddle-react';
 
-const products = [{
+const products = [
+{
 	id: 1,
 	model: 'Lobo',
 	vehicle_registration_plate: '123qwe',
-	type: 'Special',
-	tracker: 'Luis',
-	status: 'Online'}
+	type: {
+		"value": "special",
+        "label": "Special"
+	},
+	tracker: {
+		"value": "martin",
+        "label": "Martin"
+	},
+	status:{
+		"value": "off",
+        "label": "Offline"
+	},
+}
 ];
 
 const NewLayout = ({ Table }) => (
@@ -37,27 +48,26 @@ class DataTable extends Component {
 
     return (
 
-     
-
-       <Griddle data={unidades ? unidades.toJS() : [] }
-       styleConfig={{
-					classNames: {
-						Table: 'table table-striped',
-					}
-		}}
-		components={{
-			Layout: NewLayout
-		}}>
-	    <RowDefinition>
-	      <ColumnDefinition id="id" title="Unit ID" visible />
-	      <ColumnDefinition id="model" title='Model' visible/>
-	      <ColumnDefinition id="vehicle_registration_plate" title='Plate' visible />
-	      <ColumnDefinition id="type.label" title='Type' visible />
-	      <ColumnDefinition id="tracker.label" title='Tracker' visible />
-	      <ColumnDefinition id="status.label" title='Status' visible/>
-	    </RowDefinition>
-	  </Griddle>
-
+    	<div>
+	    	 <Griddle data={ unidades ? unidades.toJS() : [] }
+	       styleConfig={{
+						classNames: {
+							Table: 'table table-striped',
+						}
+			}}
+			components={{
+				Layout: NewLayout
+			}}>
+		   <RowDefinition>
+		      <ColumnDefinition id="id" title="Unit ID" visible />
+		      <ColumnDefinition id="model" title='Model' visible/>
+		      <ColumnDefinition id="vehicle_registration_plate" title='Plate' visible />
+		      <ColumnDefinition id="type.label" title='Type' visible />
+		      <ColumnDefinition id="tracker.label" title='Tracker' visible />
+		      <ColumnDefinition id="status.label" title='Status' visible/>
+		    </RowDefinition>
+		  </Griddle>
+		 </div>
     );
   }
 }
