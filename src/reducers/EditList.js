499db47425603d87f585f6e8290a0d673f5d312d
Alfredo Correa <alfredo_cc13@outlook.com>
2017-05-31 11:05:23 -0500

    change

--- src/reducers/EditList.js
+++ src/reducers/EditList.js
@@ -3,12 +3,16 @@ import { POSTED_EDIT, FETCHED_EDITED, CLEAN_DRIVER } from '../actions/IndexDrive
 
 export default (state=Immutable.List(), action) => {
 	switch(action.type) {
+
 		case POSTED_EDIT:
 			return state.merge(Immutable.List(action.payload));
+
 		case FETCHED_EDITED:
 			return Immutable.Map(action.payload);
+
 		case CLEAN_DRIVER:
 			return Immutable.Map();
+
 		default:
 			return state;
 	}
