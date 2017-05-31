8daf061ea48f1fbe716d70a2ee0aca8115d8ebe9
Alfredo Correa <alfredo_cc13@outlook.com>
2017-05-31 10:54:52 -0500

    create current marker

--- src/actions/Filters.js
+++ src/actions/Filters.js
@@ -1,5 +1,6 @@
 import qs from 'qs';
 export const FETCHED_VEHICLES_FILTER = 'FETCHED_VEHICLES_FILTER';
+export const CURRENT_MARKER = 'CURRENT_MARKER';
 
 export const fetchVehiclesFilter = (filter) => (dispatch, getStore) => fetch(`http://localhost:3004/unidades?${filter}`, {
 	method: 'GET',
@@ -7,10 +8,10 @@ export const fetchVehiclesFilter = (filter) => (dispatch, getStore) => fetch(`ht
 		'Content-Type': 'application/json'
 	},
 
-}).then( result => result.json().then( unidades => {debugger; return dispatch({
+}).then( result => result.json().then( unidades => dispatch({
 	type: FETCHED_VEHICLES_FILTER,
 	payload: unidades
 
-})}));
+})));
 
 export default fetchVehiclesFilter;
