7f6d1ecd38de4870c9a1ea767035d13da8df0524
Alfredo Correa <alfredo_cc13@outlook.com>
2017-05-31 11:04:02 -0500

    modified

--- src/reducers/DriversList.js
+++ src/reducers/DriversList.js
@@ -5,10 +5,13 @@ export default (state=Immutable.List(), action) => {
 	switch(action.type) {
 		case POSTED_DRIVER:
 			return state.merge(Immutable.List(action.payload));
+
 		case FETCHED_DRIVERS:
 			return Immutable.List(action.payload);
+
 		case DELETE_DRIVERS:
 			return state.filter(driver => driver.id !== action.payload);
+
 		default:
 			return state;
 	}
