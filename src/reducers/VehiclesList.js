5d17392f8cec0a678fc98dc82f3cb330fa74a180
Alfredo Correa <alfredo_cc13@outlook.com>
2017-05-31 11:08:54 -0500

    modified

--- src/reducers/VehiclesList.js
+++ src/reducers/VehiclesList.js
@@ -3,14 +3,19 @@ import { POSTED_VEHICLE, FETCHED_VEHICLES, DELETE_VEHICLES, UPDATE_VEHICLES } fr
 
 export default (state=Immutable.List(), action) => {
 	switch(action.type) {
+
 		case POSTED_VEHICLE:
 			return state.merge(Immutable.List(action.payload));
+
 		case FETCHED_VEHICLES:
 			return Immutable.List(action.payload);
+
 		case DELETE_VEHICLES:
 			return state.filter(vehicle => vehicle.id !== action.payload);
+
 		case UPDATE_VEHICLES:
 			return state.merge(Immutable.List(action.payload));
+			
 		default:
 			return state;
 	}
