9fc39d0de7f2f7d5289961e78b437050e3a9c0af
Alfredo Correa <alfredo_cc13@outlook.com>
2017-05-31 11:09:31 -0500

    add case current marker

--- src/reducers/receipt.js
+++ src/reducers/receipt.js
@@ -1,9 +1,13 @@
 import { fromJS } from 'immutable';
-import { FETCHED_VEHICLES_FILTER } from '../actions/Filters';
+import { FETCHED_VEHICLES_FILTER, CURRENT_MARKER } from '../actions/Filters';
 
 export default (state=fromJS([]), action) => {
 	switch(action.type) {
+
 		case FETCHED_VEHICLES_FILTER:
+			return fromJS(action.payload);
+
+		case CURRENT_MARKER:
 			return fromJS(action.payload);
 			
 		default:
