3a53b99da3c244f15f6e32facfdaa6a51717bb24
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-06-02 13:59:16 -0500

    add actions

---
+++ src/actions/indexPedidos.js
@@ -0,0 +1,12 @@
+export const POSTED_PEDIDOS = 'POSTED_PEDIDOS';
+
+export default data => (dispatch, getStore) => fetch('http://localhost:3004/pedidos', {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json'
+	},
+	body: JSON.stringify(data)
+}).then( result => result.json().then( pedidos => dispatch({
+	type: POSTED_PEDIDOS,
+	payload: pedidos
+})));
