9f24c563e0ad296500c2e5964a4dd736037dd8ef
Alfredo Correa <alfredo_cc13@outlook.com>
2017-06-07 17:34:40 -0500

    delete import googlemaploader

--- src/components/Monitoreo.js
+++ src/components/Monitoreo.js
@@ -1,26 +1,24 @@
 import { GoogleMap, Marker, withGoogleMap } from 'react-google-maps';
 import React, {PropTypes} from "react"
-import GoogleMapLoader from "react-google-maps-loader"
+
+
 const Map = withGoogleMap((props) =>{
-  
   return(
 
     <GoogleMap
-
       ref={props.onMapLoad}
       defaultZoom={13}
       defaultCenter={{lat: 19.702322, lng: -101.192190}}
-
     >
 
     {props.markers&&props.markers.map(marker=>(
       <Marker
-
         {...marker}
-
       />
     ))}
+ 
     </GoogleMap>
+
   );
 });
 export default Map;
