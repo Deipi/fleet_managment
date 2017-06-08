9dde0ee94be557b37de1e90fb52f406533054586
Alfredo Correa <alfredo_cc13@outlook.com>
2017-06-08 18:06:26 -0500

    use the font-awesome

--- src/components/SimpleFormFlotilla.js
+++ src/components/SimpleFormFlotilla.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form/immutable'
 import validate from '../validate'
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 
 const renderField = ({ input, label, type, meta: { asyncValidating, touched, error } }) => (
 	<div>
@@ -15,36 +16,42 @@ const renderField = ({ input, label, type, meta: { asyncValidating, touched, err
 const SimpleFormF = (props) => {
 	const { handleSubmit, pristine, reset, submitting, actionSubmit } = props
 	return (
-		<form onSubmit = {handleSubmit(actionSubmit)}>
-		<br/>
-			<div>
-			<center>
-			<tr>FLOTILLA</tr><br/>
-			</center>
-			</div>
+		<Row>
+			<Col className="col-sm-6 offset-3" >
+				<Card block>
+				<form onSubmit = {handleSubmit(actionSubmit)}>
+				<br/>
+					<div>
+					<center>
+					<tr>FLOTILLA</tr><br/>
+					</center>
+					</div>
 
-			<div>
-			<label>Name</label>
-			<div>
-				<Field name="nombre" component={ renderField } type="text"/>
-			</div>
+					<div>
+					<label>Name</label>
+					<div>
+						<Field name="nombre" component={ renderField } type="text"/>
+					</div>
 
-			<label>Supervisor</label>
-			<div>
-				<Field name="supervisor" component={ renderField } type="text"/>
-			</div>
+					<label>Supervisor</label>
+					<div>
+						<Field name="supervisor" component={ renderField } type="text"/>
+					</div>
 
-			<label>Garage</label>
-			<div>
-				<Field name="garage" component={ renderField } type="text"/>
-			</div>
-			</div>
+					<label>Garage</label>
+					<div>
+						<Field name="garage" component={ renderField } type="text"/>
+					</div>
+					</div>
 
-			<div>
-        		<button type="submit" disabled={pristine || submitting}>Guardar</button>
-        		<button type="button" disabled={pristine || submitting} onClick={reset}>Restablecer Valores</button>
-      		</div>
-		</form>
+					<div>
+		        		<button type="submit" disabled={pristine || submitting}>Guardar</button>
+		        		<button type="button" disabled={pristine || submitting} onClick={reset}>Restablecer Valores</button>
+		      		</div>
+				</form>
+				</Card>
+			</Col>
+		</Row>
 	)
 }
 
