085296723847cc912d525e2298431ea483989001
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-06-08 09:41:34 -0500

    add nav

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import immutable from 'immutable';
 import { combineReducers } from 'redux-immutablejs';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import { reducer as formReducer } from 'redux-form/immutable'
 import Unidad from './containers/Unidad';
 import Conductor from './containers/Conductor';
@@ -18,6 +18,10 @@ import DriversList from './containers/DriversList';
 import Contact from './containers/Contact';
 import Historial from './containers/Historial';
 import Pedidos from './containers/Pedidos';
+import Login from './containers/Login';
+import Registrar from './containers/Registrar';
+
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 import {
 	BrowserRouter as Router,
@@ -60,26 +64,87 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
 
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+        <Navbar color="faded" light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand><Link to='login'>Login</Link></NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+	            <NavItem>
+	                <NavLink><Link to='formulario'>Unidades</Link></NavLink>
+	            </NavItem>
+	            <NavItem>
+	                <NavLink><Link to='flotilla'>Flotilla</Link></NavLink>
+	            </NavItem>
+	            <NavItem>
+	                <NavLink><Link to='vehicles'>Vehicles</Link></NavLink>
+	            </NavItem>
+	            <NavItem>
+		        	<NavLink><Link to='conductores'>Conductores</Link></NavLink>
+		        </NavItem>
+		        <NavItem>
+		        	<NavLink><Link to='monitoreo'>Monitoreo</Link></NavLink>
+		        </NavItem>
+		        <NavItem>
+		        	<NavLink><Link to='drivers'>Drivers</Link></NavLink>
+		        </NavItem>
+		        <NavItem>
+		        	<NavLink><Link to='contact'>Contacto</Link></NavLink>
+		        </NavItem>
+		        <NavItem>
+		        	<NavLink><Link to='historial'>Historial</Link></NavLink>
+		        </NavItem>
+		        <NavItem>
+		        	<NavLink><Link to='pedidos'>Pedidos</Link></NavLink>
+		        </NavItem>
+		        <NavItem>
+		        	<NavLink><Link to='login'>Login</Link></NavLink>
+		        </NavItem>
+		        <NavItem>
+		        	<NavLink><Link to='registrar'>Registrar</Link></NavLink>
+	            </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+    );
+  }
+}
+
+const MenuConnect = connect()(Menu);
+
 ReactDOM.render(
   	<Provider store={ store }>
   		<Router>
 	    <div>
-	      <ul id="menu">
-	        <li><Link to='formulario'>Unidades</Link></li>
-	        <li><Link to='flotilla'>Flotilla</Link></li>
-	        <li><Link to='vehicles'>Vehicles</Link></li>
-	        <li><Link to='conductores'>Conductores</Link></li>
-	        <li><Link to='monitoreo'>Monitoreo</Link></li>
-	        <li><Link to='drivers'>Drivers</Link></li>
-	        <li><Link to='contact'>Contacto</Link></li>
-	        <li><Link to='historial'>Historial</Link></li>
-	        <li><Link to='pedidos'>Pedidos</Link></li>
-	      </ul>
+  		<MenuConnect/>
 
 	      <h2> PROYECTO </h2>
 
 	      <hr/>
 	      <Switch>
+	      	<Route path="/registrar" component={ props =>
+	      		<Component {...props}>
+	      		<Registrar/></Component> } />
+
+	      	<Route path="/login" component={ props =>
+	      		<Component {...props}>
+	      		<Login/></Component> } />
+
 	      	<Route path="/formulario" component={ props =>
 	      		<Component {...props}>
 	      		<Unidad /></Component> } />
