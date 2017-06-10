c3f524d583bb9526ff4d3d097804acb818a78e34
Alfredo Correa <alfredo_cc13@outlook.com>
2017-06-09 23:03:13 -0500

    reactstrap

--- src/components/FilterTracker.js
+++ src/components/FilterTracker.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Container, Card, CardTitle, CardText, Row, Col, InputGroup, InputGroupAddon } from 'reactstrap';
+
+const renderField = ({ onChangeAction, index, input, label, type, meta: { touched, error } }) => {
+	const styleError = {};
+	let errorSpan = null;
+
+	const ERROR_STYLE = {
+		position: 'absolute',
+		zIndex: '3',
+		right: '11px',
+		top: '-9px',
+	};
+
+	if (touched && error) {
+		errorSpan = <span className="badge badge-danger" style={ ERROR_STYLE }>{ error }</span>;
+		styleError.borderColor = 'darkred';
+	}
+	return(
+		<div style={ { position: 'relative' } }>
+		{ errorSpan }
+		<InputGroup>
+		<InputGroupAddon> {label}</InputGroupAddon>
+		<Input { ...input } style={ styleError }  name={ input.name } id="inputs" type={type} placeholder={label} />
+		</InputGroup>
+		</div>
+	);
+};
 
 export class FilterTracker extends Component {
 	render() {
 
 	return (
-
-		<FormGroup>
-			<Input type="text" name="tracker"  placeholder="Tracker..." onChange={ this.props.onTextUpdate } />
-		</FormGroup>
+		<Container>
+			<Row>
+				<InputGroup>
+				<InputGroupAddon>Tracker</InputGroupAddon>
+					<Input
+						type="text"
+						name="tracker"
+						onChange={ this.props.onTextUpdate } 
+					/>
+				</InputGroup>
+			</Row>
+		</Container>
 		);
 	}
 }
