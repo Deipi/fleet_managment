261077cda4baa0110b8bdc8a9a86522b2c397b27
Alfredo Correa <alfredo_cc13@outlook.com>
2017-06-09 23:01:21 -0500

    reactstrap

--- src/components/FilterLabel.js
+++ src/components/FilterLabel.js
@@ -2,14 +2,48 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
+import { Container, Card, CardTitle, CardText, Row, Col, InputGroup, InputGroupAddon } from 'reactstrap';
+const renderField = ({ onChangeAction, index, input, label, type, meta: { touched, error } }) => {
+	const styleError = {};
+	let errorSpan = null;
+
+	const ERROR_STYLE = {
+		position: 'absolute',
+		zIndex: '3',
+		right: '11px',
+		top: '-9px',
+	};
+
+	if (touched && error) {
+		errorSpan = <span className="badge badge-danger" style={ ERROR_STYLE }>{ error }</span>;
+		styleError.borderColor = 'darkred';
+	}
+	return(
+		<div style={ { position: 'relative' } }>
+		{ errorSpan }
+		<InputGroup>
+		<InputGroupAddon> {label}</InputGroupAddon>
+		<Input { ...input } style={ styleError }  name={ input.name } id="inputs" type={type} placeholder={label} />
+		</InputGroup>
+		</div>
+	);
+};
+
 export class FilterLabel extends Component {
 	render() {
 
 	return (
-
-		<FormGroup>
-			<Input type="text" name="label"  placeholder="Label..." onChange={ this.props.onTextUpdate } />
-		</FormGroup>
+		<Container>
+			<Row>
+				<InputGroup>
+				<InputGroupAddon>Label</InputGroupAddon>
+					<Input 
+						type="text"
+						name="label"
+						onChange={ this.props.onTextUpdate } />
+				</InputGroup>
+			</Row>
+		</Container>
 		);
 	}
 }
