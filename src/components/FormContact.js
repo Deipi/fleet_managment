ea77f026ba8b15b7eee5fa757e29a1d8a86c0c96
Alfredo Correa <alfredo_cc13@outlook.com>
2017-06-09 23:05:04 -0500

    reactstrap

--- src/components/FormContact.js
+++ src/components/FormContact.js
@@ -1,90 +1,133 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form/immutable'
-import { Row, Col, Container } from 'reactstrap'
+import { Input, Row, Col, Container, Button, InputGroup, InputGroupAddon } from 'reactstrap'
+
+const renderField = ({ onChangeAction, index, input, label, type, meta: { touched, error } }) => {
+	const styleError = {};
+	let errorSpan = null;
+
+	const ERROR_STYLE = {
+		position: 'absolute',
+		zIndex: '3',
+		right: '11px',
+		top: '-9px',
+	};
+
+	if (touched && error) {
+		errorSpan = <span className="badge badge-danger" style={ ERROR_STYLE }>{ error }</span>;
+		styleError.borderColor = 'darkred';
+	}
+	return(
+		<div style={ { position: 'relative' } }>
+		{ errorSpan }
+		<InputGroup>
+		<InputGroupAddon> {label}</InputGroupAddon>
+		<Input { ...input } style={ styleError }  name={ input.name } id="inputs" type={type} placeholder={label} />
+		</InputGroup>
+		</div>
+	);
+};
 
 const SimpleFormContac = (props) => {
   const { handleSubmit, pristine, reset, submitting } = props
   return (
-	<form onSubmit={handleSubmit}>
-		<br/>
-		<div>
-			<center>
-				<tr>CONTACTO</tr><br/>
-			</center>
-		</div>
-		<Row>
-			<Col className="col-sm-12">
-				<Col className="col-sm-6">
-					<div>
-						<label>¿QUIÉNES SOMOS?</label>
-						<div>
-							<p> DEIPI es una agencia de desarrollo de aplicaciones en Internet dedicada a
-							proveer soluciones de e-business para empresas que buscan reducir sus costos,
-							aumentar sus ingresos y mejorar su competitividad empresarial.</p>
-						</div>
-					</div>
-					<div>
-						<label>SERVICIOS</label>
-						<div>
-							<p> Ofrecemos servicios de tecnologías de información enfocadas a internet y disposivos móviles,
-							en la modalidad de SaaS (Software as a Service) y desarrollo de proyectos a la medida de las
-							necesidades de su organización.
-							Contamos con una plataforma propietaria que nos permite el desarrollo de nuevos proyectos en internet
-							de forma modular y con funcionalidad especifica.</p>
-						</div>
-					</div>
-				</Col>
-				<Col className="col-sm-6">
-					<Col className="col-sm-6">
-					  	<div>
-							<label>Nombre</label>
-							<div>
-							  	<Field name="nombre" component="input" type="text" placeholder="Nombre"/>
-							</div>
-					  	</div>
-				  	</Col>
-				  	<Col className="col-sm-6">
-						<div>
-							<label>Apellidos</label>
-							<div>
-							  	<Field name="apellidos" component="input" type="text" placeholder="Apellidos"/>
-							</div>
-					  	</div>
-				  	</Col>
-				  	<Col className="col-sm-6">
-						<div>
-							<label>Email</label>
-							<div>
-							  <Field name="email" component="input" type="email" placeholder="Email"/>
-							</div>
-					  	</div>
-			  		</Col>
-				 	<Col className="col-sm-6">
-					  	<div>
-							<label>Telefono</label>
-							<div>
-							  	<Field name="telefono" component="input" type="telefono" placeholder="Telefono"/>
-							</div>
-					  	</div>
-				  	</Col>
-				  	<Col className="col-sm-12">
-					  	<div>
-							<label>Comentario</label>
-							<div>
-							  	<Field name="comentario" component="textarea"/>
-							</div>
-					  	</div>
-				  	</Col>
-				  	<Col className="col-sm-12">
-					  	<div>
-							<button type="submit" disabled={pristine || submitting}>Enviar</button>
-							<button type="button" disabled={pristine || submitting} onClick={reset}>Reestablecer Valores</button>
-					  	</div>
-					 </Col>
-				</Col>
-			</Col>
-		</Row>
-	</form>
+  	<Container>
+
+			<form onSubmit={handleSubmit}>
+				<Row>
+
+					<Col className="offset-5">
+						<tr>CONTACTO</tr><br/>
+					</Col>
+
+					<Col className="col-sm-12">
+						<Row>
+							<Col className="col-sm-6">
+								<div>
+									<label>¿QUIÉNES SOMOS?</label>
+									<div>
+										<p> DEIPI es una agencia de desarrollo de aplicaciones en Internet dedicada a
+										proveer soluciones de e-business para empresas que buscan reducir sus costos,
+										aumentar sus ingresos y mejorar su competitividad empresarial.</p>
+									</div>
+								</div>
+								<div>
+									<label>SERVICIOS</label>
+									<div>
+										<p> Ofrecemos servicios de tecnologías de información enfocadas a internet y disposivos móviles,
+										en la modalidad de SaaS (Software as a Service) y desarrollo de proyectos a la medida de las
+										necesidades de su organización.
+										Contamos con una plataforma propietaria que nos permite el desarrollo de nuevos proyectos en internet
+										de forma modular y con funcionalidad especifica.</p>
+									</div>
+								</div>
+							</Col>
+
+							<Col className="col-sm-6">
+
+								  	<InputGroup>
+								  	<InputGroupAddon>Nombre</InputGroupAddon>
+									  	<Field 
+									  		name="nombre"
+									  		component="input"
+									  		type="text"
+									  	/>
+
+									</InputGroup>
+							  		<br/>
+
+									<InputGroup>
+									<InputGroupAddon>Apellidos</InputGroupAddon>
+
+										  	<Field
+										  		name="apellidos"
+										  		component="input"
+										  		type="text"
+										  	/>
+
+									</InputGroup>
+							  		<br/>
+
+									<InputGroup>
+									<InputGroupAddon>Email</InputGroupAddon>
+										  <Field
+										  	name="email"
+										  	component="input"
+										  	type="email"
+										  />
+
+									</InputGroup>
+						  			<br/>
+
+							 		<InputGroup>
+								  	<InputGroupAddon>Telefono</InputGroupAddon>
+										  	<Field
+										  		name="telefono"
+										  		component="input"
+										  		type="telefono"
+										  	/>
+									</InputGroup>
+							  		<br/>
+
+							  		<InputGroup>
+								  	<InputGroupAddon>Comentario</InputGroupAddon>
+										  	<Field
+										  		name="comentario"
+										  		component="textarea"
+										  	/>
+									</InputGroup>
+							  	<br/>
+
+							  	<Col className="col-sm-12">
+									<Button type="submit" color="primary" disabled={pristine || submitting}>Save</Button>
+									<Button type="button" color="primary" disabled={pristine || submitting} onClick={reset}>Clear</Button>
+								</Col>
+							</Col>
+						</Row>
+					</Col>
+				</Row>
+			</form>
+	</Container>
   )
 }
 
