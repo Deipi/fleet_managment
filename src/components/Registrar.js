73f217082ec49918c5ef94aa96b915cf88839958
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-06-12 11:14:06 -0500

    add design

--- src/components/Registrar.js
+++ src/components/Registrar.js
@@ -38,81 +38,83 @@ const SimpleFormRegistrar = (props) => {
   return (
     <Container>
         <Card block>
-        <form onSubmit={handleSubmit(actionSubmit)}>
-            <Row>
-                <Col className="col-sm-12">
-                    <center>
-                        <br/><h3>REGISTER</h3><br/>
-                    </center>
-
-                    <InputGroup>
-                        <Col className="col-sm-12">
-                            <Field
-                                name="name"
-                                component={renderField}
-                                type="text"
-                                placeholder="Nombre"
-                                label="Firts Name"
-                            />
+            <form onSubmit={handleSubmit(actionSubmit)}>
+                <Row>
+                    <Col className="col-sm-12">
+                        <Col className="offset-5">
+                            <br/><h3>REGISTER</h3><br/>
                         </Col>
-                    </InputGroup><br/>
 
-                    <InputGroup>
-                        <Col className="col-sm-12">
-                            <Field
-                                name="apellido"
-                                component={renderField}
-                                type="text"
-                                placeholder="Apellido"
-                                label="Last Name"
-                            />
+                        <Col className="col-sm-10 offset-3">
+
+                        <InputGroup>
+                            <Col className="col-sm-6">
+                                <Field
+                                    name="name"
+                                    component={renderField}
+                                    type="text"
+                                    placeholder="Nombre"
+                                    label="First Name"
+                                />
+                            </Col>
+                        </InputGroup><br/>
+
+                        <InputGroup>
+                            <Col className="col-sm-6">
+                                <Field
+                                    name="apellido"
+                                    component={renderField}
+                                    type="text"
+                                    placeholder="Apellido"
+                                    label="Last Name"
+                                />
+                            </Col>
+                        </InputGroup><br/>
+
+                        <InputGroup>
+                            <Col className="col-sm-6">
+                                <Field
+                                    name="telefono"
+                                    component={renderField}
+                                    type="telefono"
+                                    placeholder="Telefono"
+                                    label="Phone"
+                                />
+                            </Col>
+                        </InputGroup><br/>
+
+                        <InputGroup>
+                            <Col className="col-sm-6">
+                                <Field
+                                    name="email"
+                                    component={renderField}
+                                    type="email"
+                                    placeholder="Email"
+                                    label="Email"
+                                />
+                            </Col>
+                        </InputGroup><br/>
+
+                        <InputGroup>
+                            <Col className="col-sm-6">
+                                <Field
+                                    name="password"
+                                    component={renderField}
+                                    type="password"
+                                    placeholder="Password"
+                                    label="Password"
+                                />
+                            </Col>
+                        </InputGroup><br/>
+
                         </Col>
-                    </InputGroup><br/>
 
-                    <InputGroup>
-                        <Col className="col-sm-12">
-                            <Field
-                                name="telefono"
-                                component={renderField}
-                                type="telefono"
-                                placeholder="Telefono"
-                                label="Phone"
-                            />
+                        <Col className="offset-5">
+                            <Button type="submit" color="primary" onClick={()=>alert('Registrado')} disabled={pristine || submitting}>Save</Button>
                         </Col>
-                    </InputGroup><br/>
-
-                    <InputGroup>
-                        <Col className="col-sm-12">
-                            <Field
-                                name="email"
-                                component={renderField}
-                                type="email"
-                                placeholder="Email"
-                                label="Email"
-                            />
-                        </Col>
-                    </InputGroup><br/>
-
-                    <InputGroup>
-                        <Col className="col-sm-12">
-                            <Field
-                                name="password"
-                                component={renderField}
-                                type="password"
-                                placeholder="Password"
-                                label="Password"
-                            />
-                        </Col>
-                    </InputGroup><br/>
-
-                    <div>
-                        <center>
-                        <Button type="submit" color="primary" onClick={()=>alert('Registrado')} disabled={pristine || submitting}>Save</Button>
-                        </center><br/>
-                    </div>
-                </Col>
-            </Row>
-        </form>
+                    </Col>
+                </Row>
+            </form>
         </Card><br/>
     </Container>
   )
