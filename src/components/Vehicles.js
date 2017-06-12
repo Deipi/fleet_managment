5e5b92c70204f7cc5a0d249d7625b6f186e82ee6
AndreaLizbeth <andrealizbeth.cam@gmail.com>
2017-06-12 11:19:42 -0500

    modified

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -2,30 +2,20 @@ import React from 'react';
 import { Container, Card, Button, CardTitle, CardText, Row, Col, InputGroup, InputGroupAddon, Input } from 'reactstrap';
 
 export const Vehicles = props => (
-
-<Container>
-
+	<Container>
 		<Card block>
-
 			<Row>
-
 				<Col className="col-sm-6 offset-3">
-
 					<div> <center><label>DESCRIPTION</label></center> </div>
 					<div> <center>{ props.label }</center> </div>
 					<div> <center>{ props.model }</center> </div>
 					<div> <center>{ props.tracker }</center> </div>
 					<div> <center>{ props.vehicle_registration_plate }</center> </div>
 					<div> <center>{ props.type }</center> </div>
-
 				</Col>
-
 			</Row>
-
 		</Card>
-
-
-</Container>
+	</Container>
 );
 
 export default Vehicles;
