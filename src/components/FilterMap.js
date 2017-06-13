6409aea8121de2830e9580ab937456ed9a090c8e
Alfredo Correa <alfredo_cc13@outlook.com>
2017-06-13 13:24:32 -0500

    use form-control

--- src/components/FilterMap.js
+++ src/components/FilterMap.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-
 export class FilterMap extends Component {
    render() {
 		const { mapd, onClick }=this.props;
 		  return (
-		<div>
-
-			<select name='state' onChange={ mapd } >
-				<option value="">All the departments</option>
-				<option value="Desarrollo">Development</option>
-				<option value="Diseño">Design</option>
-				<option value="Ventas">Sales</option>
-			</select>
-		</div>
-
+			<div>
+				<select className="selectpicker form-control" name='state' onChange={ mapd } >
+					<option value="">All the departments</option>
+					<option value="Desarrollo">Development</option>
+					<option value="Diseño">Design</option>
+					<option value="Ventas">Sales</option>
+				</select>
+			</div>
 		);
 	}
 }
