409f664477cd99a7a5908bc9d0873b8298fb49ef
Yoli E Cald <yoxi29@hotmail.com>
2017-06-15 17:13:56 -0500

    added styles for burger

--- src/containers/Alertas.js
+++ src/containers/Alertas.js
@@ -2,71 +2,71 @@ import React from 'react'
 import Alertas from '../components/Alertas';
 import { connect } from 'react-redux';
 import createAlertas from '../actions/indexAlertas';
-import { push as Menu } from 'react-burger-menu';
+import { push as Menu } from 'react-burger-menu'; 
+import './burger.css'
 
 const styles = {
   bmBurlefttton: {
-    position: 'fixed',
-    width: '36px',
-    height: '30px',
-    left: '36px',
-    top: '36px'
+	position: 'fixed',
+	width: '36px',
+	height: '30px',
+	left: '36px',
+	top: '36px'
   },
   bmBurgerBars: {
-    background: '#373a47'
+	background: '#373a47'
   },
   bmCrossButton: {
-    height: '24px',
-    width: '24px'
+	height: '24px',
+	width: '24px'
   },
   bmCross: {
-    background: '#bdc3c7'
+	background: '#bdc3c7'
   },
   bmMenu: {
-    background: '#373a47',
-    padding: '2.5em 1.5em 0',
-    fontSize: '1.15em'
+	background: '#373a47',
+	padding: '2.5em 1.5em 0',
+	fontSize: '1.15em'
   },
   bmMorphShape: {
-    fill: '#373a47'
+	fill: '#373a47'
   },
   bmItemList: {
-    color: '#b8b7ad',
-    padding: '0.8em'
+	color: '#b8b7ad',
+	padding: '0.8em'
   },
   bmOverlay: {
-    background: 'rgba(0, 0, 0, 0.3)'
+	background: 'rgba(0, 0, 0, 0.3)'
   }
+
 };
 
 class AlertasComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.actionSubmit = this.actionSubmit.bind(this);
+	constructor(props) {
+		super(props);
+		this.actionSubmit = this.actionSubmit.bind(this);
 }
 
-    actionSubmit() {
-        alert('YA');
-        const { dispatch } = this.props;
-    }
+	actionSubmit() {
+		alert('YA');
+		const { dispatch } = this.props;
+	}
 
-    render() {
-        const { dispatch } = this.props;
-        return (
-            <div>
-                <Menu
-                    isOpen={ true }
-                    width={ 280 }
-                    styles={ styles }
-                    left
-                >
-                    <a id="home" className="menu-item" href="/">Home</a>
-                    <a id="about" className="menu-item" href="/about">About</a>
-                    <a id="contact" className="menu-item" href="/contact">Contact</a>
-                </Menu>
-            </div>
-        );
-    }
+	render() {
+		const { dispatch } = this.props;
+		return (
+				<Menu
+					isOpen={ true }
+					width={ 280 }
+					styles={ styles }
+					left
+				>
+					<a id="home" className="menu-item" href="/">Home</a>
+					<a id="about" className="menu-item" href="/about">About</a>
+					<a id="contact" className="menu-item" href="/contact">Contact</a>
+				</Menu>
+		);
+	}
 }
 
 export default connect()(AlertasComponent);
