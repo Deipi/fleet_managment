801f8334116adf71762a4f6c65708390e9b3c021
Yoli E Cald <yoxi29@hotmail.com>
2017-06-15 18:00:42 -0500

    updated table Vehicles

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Container, Card, Button, CardTitle, CardText, Row, Col, InputGroup, InputGroupAddon, Input } from 'reactstrap';
+import { Container, CardHeader,Card, Button, CardTitle, CardText, Row, Col, InputGroup, InputGroupAddon, Input } from 'reactstrap';
 
 export const Vehicles = props => (
-	<Container>
-		<Card block>
-			<Row>
-				<Col className="col-sm-6 offset-3">
-					<div> <center><label>DESCRIPTION</label></center> </div>
-					<div> <center>{ props.label }</center> </div>
-					<div> <center>{ props.model }</center> </div>
-					<div> <center>{ props.tracker }</center> </div>
-					<div> <center>{ props.vehicle_registration_plate }</center> </div>
-					<div> <center>{ props.type }</center> </div>
-				</Col>
-			</Row>
-		</Card>
+	<Container className="mt-2">
+		<Col className="col-sm-6 offset-3">
+			<Card>
+			<CardHeader className="text-center">DESCRIPTION </CardHeader>	
+				<div className="text-center mt-1"> { props.label } </div>
+				<div className="text-center mt-1"> { props.model } </div>
+				<div className="text-center mt-1"> { props.tracker } </div>
+				<div className="text-center mt-1"> { props.vehicle_registration_plate } </div>
+				<div className="text-center mt-1"> { props.type } </div>
+			</Card>
+		</Col>
 	</Container>
 );
 
